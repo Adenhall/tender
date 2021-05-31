fe7707af70648f177e0bb4bd82866f296e44dc34
Quan Nguyen <nguyen.mq@allerin.nl>
2021-05-31 22:02:23 +0700

    Added react routing and pages

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import UserCard from 'components/UserCard'
+
+const Dashboard = () => {
+    return (
+        <div>
+            This is a dashboard
+            <UserCard />
+        </div>
+    )
+}
+
+export default Dashboard
